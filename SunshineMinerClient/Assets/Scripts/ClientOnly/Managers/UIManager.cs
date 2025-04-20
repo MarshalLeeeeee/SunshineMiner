eb92ee7db4ae4ff4c9e195fec49799602c10a578
MarshalLeeeeee <marshallee413lmc@sina.com>
2025-04-20 22:49:37 +0800

    Add: uimanager and panel loading

---
+++ SunshineMinerClient/Assets/Scripts/ClientOnly/Managers/UIManager.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using System.Threading.Tasks;
+using static UnityEditor.Experimental.GraphView.GraphView;
+using UnityEngine.UI;
+
+public enum CanvasLayer
+{
+    Background = 0,
+    Default = 100,
+    Popup = 200,
+    Loading = 300,
+    Alert = 400,
+    Debug = 1000
+}
+
+
+public class UIManager : MonoBehaviour
+{
+    // Singleton instance
+    public static UIManager Instance { get; private set; }
+    private Dictionary<CanvasLayer, Canvas> canvases = new Dictionary<CanvasLayer, Canvas>();
+    private AsyncOperationHandle<GameObject> panelHandle;
+    [SerializeField] private string pnlStartName;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        // Load PnlStart
+        LoadPanelAsync(pnlStartName, CanvasLayer.Default);
+    }
+
+    public void LoadPanelAsync(string pnlName, CanvasLayer layer)
+    {
+        StartCoroutine(LoadPanelWrapper(pnlName, layer));
+    }
+
+    private IEnumerator LoadPanelWrapper(string pnlName, CanvasLayer layer)
+    {
+        var task = DoLoadPanelAsync(pnlName, layer);
+        yield return new WaitUntil(() => task.IsCompleted);
+
+        if (task.IsFaulted)
+        {
+            Debug.Log($"Load panel {pnlName} failed");
+        }
+    }
+
+    public async Task DoLoadPanelAsync(string pnlName, CanvasLayer layer)
+    {
+        string pnlPath = $"Assets/UI/Panels/{pnlName}.prefab";
+        panelHandle = Addressables.LoadAssetAsync<GameObject>(pnlPath);
+        await panelHandle.Task;
+
+        if (panelHandle.Status == AsyncOperationStatus.Succeeded)
+        {
+            Canvas canvas = GetCanvas(layer);
+            GameObject currentPanel = Instantiate(panelHandle.Result, canvas.transform);
+            currentPanel.transform.localPosition = Vector3.zero;
+            currentPanel.transform.localScale = Vector3.one;
+        }
+        else
+        {
+            Debug.LogError($"Load {pnlPath} failed: {panelHandle.OperationException}");
+        }
+    }
+
+    private Canvas GetCanvas(CanvasLayer layer)
+    {
+        if (!canvases.ContainsKey(layer))
+        {
+            return CreateCanvas(layer);
+        }
+        else
+        {
+            return canvases[layer];
+        }
+    }
+
+    private Canvas CreateCanvas(CanvasLayer layer)
+    {
+        GameObject canvasGO = new GameObject($"Canvas_{layer}");
+        Canvas canvas = canvasGO.AddComponent<Canvas>();
+        canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+        canvas.sortingOrder = (int)layer;
+        canvasGO.AddComponent<GraphicRaycaster>();
+        canvases[layer] = canvas;
+        return canvas;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
